868777b1a6a8b8b4eec067d8a4a8e9c522d677f9
RanolP <public.ranolp@gmail.com>
2022-06-17 13:50:42 +0900

    CORS

--- src/util/web.ts
+++ src/util/web.ts
@@ -4,6 +4,22 @@ type Handler = (req: VercelRequest, res: VercelResponse) => void;
 
 export function route(f: (req: VercelRequest) => Promise<unknown>): Handler {
   return (req, res) => {
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    // another common pattern
+    // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+    res.setHeader(
+      'Access-Control-Allow-Methods',
+      'GET,OPTIONS,PATCH,DELETE,POST,PUT',
+    );
+    res.setHeader(
+      'Access-Control-Allow-Headers',
+      'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version',
+    );
+    if (req.method === 'OPTIONS') {
+      res.status(200).end();
+      return;
+    }
     f(req)
       .then((output) => res.json(output))
       .catch((err) => res.send(err));
